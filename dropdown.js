ef083a052f7e0b2859812b38214a0e27b4c7505c
aacaskie <austincaskie7@gmail.com>
2024-11-20 15:30:57 -0500

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -46,27 +46,21 @@ function includeHTML() {
   }
 }
 /*slide show code*/
-let slideIndex = 1;
-showSlides(slideIndex);
+const els = (sel, par = document) => par.querySelectorAll(sel);
+const el = (sel, par = document) => par.querySelector(sel);
+const mod = (n, m) => (n % m + m) % m;
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("gslides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+els(".slider-wrapper").forEach(elPar => {
+  const elSlider = el(".slider", elPar);
+  const elsItems = els(".item", elPar);
+  const sub = +elPar.dataset.items ?? 1;
+  const tot = Math.ceil(elsItems.length / sub);
+  let c = 0;
+  
+  const anim = () => elSlider.style.transform = `translateX(-${c*100}%)`;
+  const prev = () => (c = mod(c-1, tot), anim());
+  const next = () => (c = mod(c+1, tot), anim());
+  
+  el(".prev", elPar).addEventListener("click", prev);
+  el(".next", elPar).addEventListener("click", next);
+});
