8a59dba3749d872a3ee96d6e0d14a0c6a28a9fb6
aacaskie <austincaskie7@gmail.com>
2024-11-20 20:08:06 -0500

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -17,10 +17,9 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-    slides[2].style.display = "block";
+     slides[i].style.display = "none";
   }
-  slides[2].style.display = "block";
+  slides[slideIndex -1].style.display = "block";
 }
 
 /* include html*/
